d0c5a26e383398c0a06ce4a41c888592cc2029cf
AlgoWebBot <shimulzahan636@gmail.com>
2024-03-15 19:26:07 +0600

    resolve murge conflicts

---
+++ src/Components/Categories.jsx/Categories.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+
+
+
+const Categories = () => {
+
+
+
+
+    const menus = [
+
+        {
+
+            title: 'Shop Surface devices',
+
+            image: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/Link-List-Icons-Surface-Devices?wid=40&hei=40",
+
+            path: '/shop-surface'
+
+        },
+
+        {
+
+            title: 'Choose your Microsoft 365',
+
+            image: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/Link-List-Icons-Microsoft-365?wid=40&hei=40",
+
+            path: '/ms-365'
+
+        },
+
+        {
+
+            title: 'Shop for your business ',
+
+            image: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/Link-List-Icons-Business?wid=40&hei=40",
+
+            path: '/shop-business'
+
+        },
+
+        {
+
+            title: 'Shop for accessories',
+
+            image: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/Link-List-Icons-Accessories?wid=40&hei=40",
+
+            path: '/shop-accessories'
+
+        },
+
+        {
+
+            title: 'Shop Xbox games and consoles',
+
+            image: "https://cdn-dynmedia-1.microsoft.com/is/content/microsoftcorp/Link-List-Icons-Xbox-Games-Consoles?wid=40&hei=40",
+
+            path: '/xbox'
+
+        },
+
+    ]
+
+
+
+
+    return (
+
+        <div className='pt-10 pb-20'>
+
+            <div className='flex flex-col lg:flex-row justify-center items-center gap-10'>
+
+                {
+
+                    menus.map((menu, idx) =>
+
+                        <Link key={idx} to={menu.path} className='flex justify-center items-center flex-col gap-2'>
+
+                            <img src={menu.image} alt="cat-image" className='h-10 w-10' />
+
+                            <h1 className='font-bold underline text-blue-700'>{menu.title}</h1>
+
+                        </Link>
+
+                    )
+
+                }
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+
+
+
+export default Categories
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import Footer from '../../Components/Footer/Footer'
 import DeskNav from '../../Components/Navbar/DeskNav'
 import Slider from '../Slider/Slider'
 import Banner from '../Banner/Banner'
+import Card from '../../Components/Card/Card'
+import Categories from '../../Components/Categories.jsx/Categories'
 
 const Home = () => {
     return (
@@ -12,7 +14,31 @@ const Home = () => {
                 <Nav />
                 <DeskNav />
                 <Slider />
+                <Categories />
+                <div className='grid grid-cols-1 lg:grid-cols-4 justify-center items-center gap-8 w-full px-10 py-10'>
+                    <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Test-Surface-Laptop-5-Sandstone-02?wid=380&hei=213&fit=crop"} />
+                    <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Surface-Laptop-Studio-2-CONS-M001?wid=380&hei=213&fit=crop"} />
+                    <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Microsoft-365-Lifestyle-Sandstone-Icon-Toss?wid=380&hei=213&fit=crop"} />
+                    <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-XSX-CP-Xbox-Series-X?wid=380&hei=213&fit=crop"} />
+                </div>
                 <Banner />
+                <div className='px-10 py-10'>
+                    <h1 className='text-4xl font-bold mb-7'>For business</h1>
+                    <div className='grid grid-cols-1 lg:grid-cols-4 justify-center items-center gap-8 w-full'>
+                        <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Surface-Laptop-5-LIfestyle-M2?wid=380&hei=213&fit=crop"} />
+                        <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Copilot-Commercial?wid=380&hei=213&fit=crop"} />
+                        <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-CP-Microsoft-Teams-Commercial?wid=380&hei=213&fit=crop"} />
+                        <Card path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Copilot-For-Security?wid=380&hei=213&fit=crop"} />
+                    </div>
+                </div>
+                <div className='px-10 py-10'>
+                    <h1 className='text-4xl font-bold mb-7'>Explore more about AI and Copilot</h1>
+                    <div className='grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-8 w-full'>
+                        <Card size={'large'} path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-AI-Ten-Terms?wid=517&hei=291&fit=crop"} />
+                        <Card size={'large'} path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Copilot-Innovative-Uses-Purple?wid=517&hei=291&fit=crop"} />
+                        <Card size={'large'} path={''} image={"https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-dAI-in-the-Life-Liam?wid=517&hei=291&fit=crop"} />
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
